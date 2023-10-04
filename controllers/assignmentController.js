ed24dc4d0e72d160e07b29170771a0b6d7c7df7c
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-04 17:28:25 -0400

    tests and workflow added

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -293,4 +293,3 @@ export const healthz = async (request, response) => {
 
 
 
-
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import config from './config/dbConfig.js';
 
 const port = 8080;  //initialized a port to be used
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);   //main app.js file listens to the server.
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
+
+export default server;
