415283d3f9cb105ed82c273c7e7be87dad35cf9f
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-16 16:46:56 -0400

    change in value sh - testing

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$PASSWORD');FLUSH PRIVILEGES;"
+sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$MYSQL_ROOT_PASSWORD');FLUSH PRIVILEGES;"
 printf '$MYSQL_ROOT_PASSWORD\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
 sudo mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -Bse "CREATE DATABASE $MYSQL_DATABASE;"
