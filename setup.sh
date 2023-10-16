6220b7f80ee35b2d8f1e2b949ce4f32206899ff2
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-16 16:40:34 -0400

    trying sh file with secret keys -testing

--- setup.sh
+++ setup.sh
@@ -13,10 +13,10 @@ sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$PASSWORD');FLUSH PRIVILEGES;"
-printf '$PASSWORD\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$PASSWORD';"
-mysql -u root -p$PASSWORD -Bse "CREATE DATABASE $DATABASE;"
-mysql -u root -p$PASSWORD -Bse "SHOW DATABASES;"
+printf '$MYSQL_ROOT_PASSWORD\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -Bse "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
 sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
