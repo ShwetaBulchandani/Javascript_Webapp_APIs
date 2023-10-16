04c945f936f5a2799595a731f10532f1e812f3e6
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-16 18:06:38 -0400

    Demo (#25)
    * Dev (#3)
    
    * First commit with all the APIs
    
    * Dev (#5)
    
    * First commit with all the APIs
    
    * tests and workflow added
    
    * Updated test case and package json update
    
    * Updated test case and package json update - env
    
    * port changes and testing
    
    * port changes and testing- solving error
    
    * Dev (#6)
    
    * First commit with all the APIs
    
    * tests and workflow added
    
    * Updated test case and package json update
    
    * Updated test case and package json update - env
    
    * port changes and testing
    
    * port changes and testing- solving error
    
    * Create README.md
    
    * path change
    
    * final changes
    
    * final changes  ss
    
    * Update README.md
    
    * Update README.md
    
    * Update user.test.js
    
    * Update user.test.js
    
    * workflow packer change test
    
    * adding packer hcl file test on demo branch
    
    * format check test demo packer file
    
    * testing workflow
    
    * change in build workflow test
    
    * change in build_workflow yml
    
    * updating build workflow test on demo
    
    * trying env in bash script - test
    
    * update in hcl file test
    
    * test hcl
    
    * test hcl packer unzip
    
    * update test file
    
    * trying sh file with secret keys -testing
    
    * change in value sh - testing
    
    * changes - testing
    
    * trying test
    
    * changes - testing
    
    * test
    
    * test

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+
 # Check if running in GitHub Actions
-# if [ -n "${GITHUB_ACTIONS}" ]; then
-#   # In GitHub Actions, copy the content of .env directly
-#   echo "${{ secrets.MYSQL_ROOT_PASSWORD }}" > .env
-#   echo "${{ secrets.MYSQL_DATABASE }}" > .env
-# fi
+
+if [ -n "${GITHUB_ACTIONS}" ]; then
+  # In GitHub Actions, copy the content of .env directly
+  echo "password=${{ secrets.PASSWORD }}" > .env
+  echo "database=${{ secrets.DATABASE }}" >> .env
+fi
 
 if [ -f .env ]; then
   # Load environment variables from .env file
@@ -22,11 +24,11 @@ sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$MYSQL_ROOT_PASSWORD');FLUSH PRIVILEGES;"
-printf '$MYSQL_ROOT_PASSWORD\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -Bse "CREATE DATABASE $MYSQL_DATABASE;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -Bse "SHOW DATABASES;"
+sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$PASSWORD');FLUSH PRIVILEGES;"
+printf '$PASSWORD\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$PASSWORD';"
+mysql -u root -p$PASSWORD -Bse "CREATE DATABASE $DATABASE;"
+mysql -u root -p$PASSWORD -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
 sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
