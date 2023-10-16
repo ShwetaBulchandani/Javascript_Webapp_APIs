1f7c8638e4aa273f31bf9e36e3a6815ad1ee46c5
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-16 16:07:24 -0400

    trying env in bash script - test

--- setup.sh
+++ setup.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
+
+if [ -f .env ]; then
+  source .env
+else
+  echo "Error: .env file not found"
+  exit 1
+fi
+
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('9822684461mM@12');FLUSH PRIVILEGES;"
-printf '9822684461mM@12\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON cloud_db.* TO 'root'@'localhost' IDENTIFIED BY '9822684461mM@12';"
-mysql -u root -p9822684461mM@12 -Bse "CREATE DATABASE cloud_db;"
-mysql -u root -p9822684461mM@12 -Bse "SHOW DATABASES;"
+sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$PASSWORD');FLUSH PRIVILEGES;"
+printf '$PASSWORD\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$PASSWORD';"
+mysql -u root -p$PASSWORD -Bse "CREATE DATABASE $DATABASE;"
+mysql -u root -p$PASSWORD -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
 sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
