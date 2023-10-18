bde842647553137dd68282307e5767c841411a73
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-18 12:44:39 -0400

    Demo (#50)
    * Dev (#3)
    
    * First commit with all the APIs
    
    * Dev (#5)
    
    * First commit with all the APIs
    
    * tests and workflow added
    
    * Updated test case and package json update
    
    * Updated test case and package json update - env
    
    * port changes and testing
    
    * port changes and testing- solving error
    
    * Dev (#6)
    
    * First commit with all the APIs
    
    * tests and workflow added
    
    * Updated test case and package json update
    
    * Updated test case and package json update - env
    
    * port changes and testing
    
    * port changes and testing- solving error
    
    * Create README.md
    
    * path change
    
    * final changes
    
    * final changes  ss
    
    * Update README.md
    
    * Update README.md
    
    * Update user.test.js
    
    * Update user.test.js
    
    * workflow packer change test
    
    * adding packer hcl file test on demo branch
    
    * format check test demo packer file
    
    * testing workflow
    
    * change in build workflow test
    
    * change in build_workflow yml
    
    * updating build workflow test on demo
    
    * trying env in bash script - test
    
    * update in hcl file test
    
    * test hcl
    
    * test hcl packer unzip
    
    * update test file
    
    * trying sh file with secret keys -testing
    
    * change in value sh - testing
    
    * changes - testing
    
    * trying test
    
    * changes - testing
    
    * test
    
    * test
    
    * test
    
    * test aws
    
    * test aws
    
    * test aws hcl
    
    * changes in hcl and sh
    
    * changes testing
    
    * changes with filter
    
    * test
    
    * github workflow updated
    
    * testing
    
    * testing
    
    * test
    
    * testing code
    
    * test
    
    * yml workflow change - test
    
    * updated sh file and hcl file
    
    * userservice change path -testing
    
    * changes in webapp and testing
    
    * user test case change -- testing

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../server.js';
 import { expect } from 'chai';
 
-describe("GET /v1/healthz", () => {
+describe("GET /healthz", () => {
   it("It should respond 200", (done) => {
     request(app)
       .get("/v1/healthz")
