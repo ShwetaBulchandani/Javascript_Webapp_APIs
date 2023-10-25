e2dfcab55c1f64551cd6655307e0beef11f6cf21
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-25 19:43:43 -0400

    change for the path -testing

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
-sudo mkdir opt
-sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
-sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
-cd opt
-sudo unzip -o webapp.zip
-cd webapp
-sudo npm i
+
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
+sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
+sudo mv /home/admin/users.csv /opt/csye6225/users.csv
+cd /opt/csye6225
+sudo unzip -o webapp.zip
+sudo npm i
 sudo cp /home/admin/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
