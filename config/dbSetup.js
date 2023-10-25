86e1720d76573975c1b1b64d6c73ffcc2299c796
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-25 14:09:21 -0400

    testing

--- config/dbSetup.js
+++ config/dbSetup.js
@@ -3,9 +3,14 @@ import config from '../config/dbConfig.js';
 import assignmentModel from '../models/assignmentModel.js';
 import userModel from '../models/userModel.js';
 
-const sequelize = new Sequelize(
-  `${config.database.dialect}://${config.database.user}:${config.database.pd}@${config.database.host}/${config.database.database}`
-);
+// const sequelize = new Sequelize(
+//   `${config.database.dialect}://${config.database.user}:${config.database.pd}@${config.database.host}/${config.database.database}`
+// );
+
+const { dialect, host, user, password, database } = config.database;
+
+const sequelize = new Sequelize(`${dialect}://${user}:${password}@${host}/${database}`);
+
 
 const db = {
   sequelize,
