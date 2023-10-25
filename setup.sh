0374541b8a42dae41da87588b8cc3aae06a6d998
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-10-25 00:04:29 -0400

    assignment 6 changes

--- setup.sh
+++ setup.sh
@@ -4,12 +4,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
-sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$PASSWORD');FLUSH PRIVILEGES;"
-printf '$PASSWORD\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USER'@'localhost' IDENTIFIED BY '$PASSWORD';"
-mysql -u root -p$PASSWORD -Bse "CREATE DATABASE $DATABASE;"
-mysql -u root -p$PASSWORD -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
 sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
@@ -17,6 +11,7 @@ cd opt
 sudo unzip -o webapp.zip
 cd webapp
 sudo npm i
-sudo npm rebuild bcrypt
-sudo npm run test
-# sudo npm start
+sudo cp /home/admin/aws-debian.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable aws-debian.service
+sudo systemctl start aws-debian.service
