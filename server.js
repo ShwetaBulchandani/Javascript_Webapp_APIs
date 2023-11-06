46a1f6cbf0a348dc6a9a05a458dd989fb70e01ee
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-11-06 16:44:02 -0500

    change in path of logger

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './app.js';  //imported app from app.js
 import config from './config/dbConfig.js';
-import logger from '../config/dbSetup.js'; 
+import logger from './config/dbSetup.js'; 
 
 const port = 8080;  //initialized a port to be used
 
