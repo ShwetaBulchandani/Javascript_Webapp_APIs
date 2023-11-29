201444d5f50dd85c601f6ea34ee18b9e7878d146
ShwetaBulchandani <114502096+ShwetaBulchandani@users.noreply.github.com>
2023-11-29 04:53:30 -0500

    change in submission api

--- models/submissionModel.js
+++ models/submissionModel.js
@@ -1,46 +1,43 @@
 import { DataTypes } from "sequelize";
 
 const submissionModel = (sequelize) => {
-    const Submission = sequelize.define(
-        'submission',
-        {
-            id: {
-                type: DataTypes.UUID,
-                defaultValue: DataTypes.UUIDV4,
-                primaryKey: true,
-                allowNull: false,
-            },
-            user_id: {
-                type: DataTypes.UUID,
-                allowNull: false,
-            },
-            assignment_id: {
-                type: DataTypes.UUID,
-                allowNull: false,
-            },
-            submission_url: {
-                type: DataTypes.STRING,
-                validate: {
-                    isUrl: true,
-                },
-                allowNull: false,
-            },
-            submission_date: {
-                type: DataTypes.DATE,
-                allowNull: false,
-            },
-            submission_updated: {
-                type: DataTypes.DATE,
-                allowNull: false,
-            },
+  const Submission = sequelize.define(
+    "submission",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      assignment_id: {
+        type: DataTypes.UUID,
+      },
+      user_id: {
+        type: DataTypes.UUID,
+      },
+      submission_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isUrl: true,
         },
-        {
-            timestamps: false,
-            underscored: true,
-        }
-    );
+      },
+      attempts: {
+        type: DataTypes.INTEGER,
+      },
+      submission_date: {
+        type: DataTypes.DATE,
+      },
+      submission_updated: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 
-    return Submission;
+  return Submission;
 };
 
 export default submissionModel;
